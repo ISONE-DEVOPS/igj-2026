2d4e6661059344ff8953c48b7c2bb0ca74733afb
Linda Peixoto <linda.peixoto@pagali.cv>
2026-03-13 01:12:59 -0100

    Header: novo logo IGJ com texto

--- website/src/components/public/Header.tsx
+++ website/src/components/public/Header.tsx
@@ -70,7 +70,7 @@ export function Header() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-3 group">
-            <img src="/igj-logo-dark.png" alt="IGJ - Inspecção-Geral de Jogos" className="h-10 w-auto" />
+            <img src="/logo-igj.png" alt="IGJ - Inspecção Geral de Jogos" className="h-12 w-auto" />
           </Link>
 
           {/* Desktop nav */}
